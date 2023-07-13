f25e4177ab1c245c2004a2aa04305e89ebc811e6
Raven Spencer <107158921+RavenEsc@users.noreply.github.com>
2023-07-13 18:00:55 -0500

    test

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "this" {
+  bucket = "My-Terraform-Cloud-Bucket"
+}
