db65379ffd6702942552c7751993c4c768705d0a
Raven Spencer <107158921+RavenEsc@users.noreply.github.com>
2023-07-14 12:54:53 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,20 @@
 terraform {
+  required_version = ">= 0.12"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
   }
-  cloud {
-    organization = "raven-for-aws"
 
-    workspaces {
-    name = "OIDC-test-configuration"
+  cloud {
+    remote_state {
+      organization = "raven-for-aws"
+
+      workspaces {
+        name = "OIDC-test-configuration"
+      }
     }
    }
 }
@@ -17,6 +22,10 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
+
+  assume_role {
+    role_arn = "arn:aws:iam::464004139021:role/TerraformCloudassumerole"
+  }
 }
 
 resource "aws_s3_bucket" "this" {
